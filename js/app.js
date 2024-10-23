3af4d8a17b9bdc6d18ca679b9d41ebd0b41d1332
yomnaa3 <132593493+yomnaa3@users.noreply.github.com>
2024-10-23 04:09:32 +0300

    navbar menu Creation, Navbar responsiveness

--- js/app.js
+++ js/app.js
@@ -1,67 +1,67 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
-*/
+ *
+ */
 
 /**
  * Comments should be present at the beginning of each procedure and class.
  * Great to have comments before crucial code sections within the procedure.
-*/
+ */
 
 /**
  * Define Global Variables
- * 
-*/
-const navbarList =document.getElementById('navbar__list') ;
-const sections = document.querySelectorAll('section');
+ *
+ */
+const navbarList = document.getElementById("navbar__list");
+const sections = document.querySelectorAll("section");
 /**
  * End Global Variables
  * Start Helper Functions
- * 
-*/
+ *
+ */
 sections.forEach(function (section) {
-    const listItem = document.createElement('li');
-    listItem.classList.add('menu__link');
-    listItem.innerHTML = `<a href="#${section.id}">${section.dataset.nav}</a>`;
-    navbarList.appendChild(listItem);
-
-})
+  const listItem = document.createElement("li");
+  // listItem.classList.add('menu__link');
+  listItem.innerHTML = `<a class="menu__link" href="#${section.id}">${section.dataset.nav}</a>`;
+  navbarList.appendChild(listItem);
+});
 
 /**
  * End Helper Functions
  * Begin Main Functions
- * 
-*/
+ *
+ */
 
 // build the nav
 
-
 // Add class 'active' to section when near top of viewport
 
-
 // Scroll to anchor ID using scrollTO event
-
 
 /**
  * End Main Functions
  * Begin Events
- * 
-*/
+ *
+ */
 
-// Build menu 
+// Build menu
 
 // Scroll to section on link click
 
 // Set sections as active
 
+const nav__btn = document.querySelector(".navbar__menu-btn");
 
+nav__btn.addEventListener("click", () => {
+    navbarList.classList.toggle("active");
+});
